cadf69e6c6bfb8bd871592012e5573767ef033dc
Daniel Thompson <Daniel Thompson>
2016-04-25 22:43:33 +0100

    Gelbooru and hug

---
+++ Mikibot/Extensions/Gelbooru/GelbooruPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.Extensions.Gelbooru
+{
+    class GelbooruPost
+    {
+        public string directory;
+        public string hash;
+        public string height;
+        public string image;
+        public string change;
+        public string owner;
+        public string parent_id;
+        public string rating;
+        public string sample;
+        public string sample_height;
+        public string sample_width;
+        public string score;
+        public string tags;
+        public string width;
+        public string file_url;
+    }
+}
